55f2d81ce28a0cb069a6df9a24a80d830ac29a4a
Mumtaz Abdrizack Mohamud <mumtaz.mohamud@edu.edugrade.se>
2022-10-10 17:13:58 +0200

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop___Lab_5
+{
+    //Mumtaz MOhamud [NET22]
+    class Circle
+    {
+        //Saves variables to call it later.
+        float _pi = 3.141f;
+        int _Radius;
+        //Constructor
+        public Circle(int _Radius)
+        {
+            this._Radius = _Radius;
+        }
+        //Method that retuns area
+        public double getArea()
+        {
+            double Area = _Radius * _Radius * _pi;
+            return Area;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop___Lab_5
+{
+    //Mumtaz MOhamud [NET22]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calls the constructor and gives an output.
+            Circle C1 = new Circle(5);
+            Console.WriteLine("Arean av cirkel 1  är : " + C1.getArea());
+            Circle C2 = new Circle(6);
+            Console.WriteLine("Area av cirkel 2 är : " + C2.getArea());
+        }
+
+    }
+
+
+
+
+
+}
